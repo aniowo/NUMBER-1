91a6bf3434ef1ea6e98f7c1e281ae96139d44cbd
aniowo <73297528+aniowo@users.noreply.github.com>
2020-11-10 08:19:23 +0100

    Add files via upload

---
+++ NUMBER 1/NUMBER 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUMBER_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Enter the string: ");
+                string SentenceString = Console.ReadLine();
+
+                var list = SentenceString.ToLower().GroupBy(s => s).ToList();
+
+                foreach (var c in list)
+                {
+                    Console.WriteLine("Character " + c.Key + ":" + c.Count() + " times");
+                }
+
+            if (string.IsNullOrWhiteSpace(SentenceString))
+            {
+                Console.WriteLine("ERROR IN INPUT");
+            }
+            Console.ReadKey();
+            }
+    }
+}
